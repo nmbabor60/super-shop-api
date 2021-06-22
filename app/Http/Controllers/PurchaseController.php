9dcdfdb6d1877abc5daa331f735fb91ddcb34963
nmbabor <nmbabor60@gmail.com>
2021-06-22 20:22:06 +0600

    Purchase Product Update(Inventory & Challan, Add new column in challan)

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -98,6 +98,7 @@ class PurchaseController extends Controller
                 'big_unit_qty'=>$item->big_unit_qty??null,
                 'small_unit_qty'=>$item->small_unit_qty
             ];
+
             $purchaseItem = PurchaseItem::create($purchaseItemInput);
             // Start Inventory/Stock
             $existProduct = Inventory::where('product_id',$item->product_id)->first();
@@ -124,6 +125,7 @@ class PurchaseController extends Controller
                 $inventory = Inventory::create($inventoryInput);
             }
             $inventoryChallan = [
+                'purchase_id'=>$purchase->id,
                 'inventory_id'=>$inventory->id,
                 'product_id'=>$item->product_id,
                 'big_unit_sales_price'=>$item->big_unit_sales_price?? null,
@@ -165,7 +167,141 @@ class PurchaseController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        /*
+        {
+            "purchase_date":"2021-05-26",
+            "challan_no":"r2424",
+            "supplier_id":2,
+            "note":"First Purchase",
+            "total_amount":106000,
+            "created_by":1,
+            "items":[
+                {
+                    "product_id":1,
+                    "small_unit_price":21000,
+                    "small_unit_qty":4,
+                    "small_unit_sales_price":22000,
+
+                    "big_unit_price":21000 (nullable),
+                    "big_unit_qty":4  (nullable),
+                    "big_unit_sales_price":22000  (nullable)
+                },
+                {
+                    "product_id":2,
+                    "small_unit_price":11000,
+                    "small_unit_qty":2,
+                    "small_unit_sales_price":12000
+                }
+            ]
+        }
+
+         */
+        DB::beginTransaction();
+        try {
+            $input = $request->all();
+            //return response()->json($input,201);
+            $validator = \Validator::make($input, [
+                'total_amount' => 'required',
+                'supplier_id' => 'required',
+                'challan_no' => 'required',
+                'items.*.product_id' => 'required',
+                'items.*.small_unit_qty' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 403);
+            }
+
+            /* Start Purchase  */
+            $purchaseInput = [
+                'purchase_date'=>date('Y-m-d',strtotime($request->purchase_date)),
+                'challan_no'=>$request->challan_no,
+                'supplier_id'=>$request->supplier_id,
+                'note'=>$request->note ?? '',
+                'total_amount'=>$request->total_amount ,
+            ];
+            $purchase = Purchase::findOrFail($id);
+            $purchase->update($purchaseInput);
+            // End Purchase Table Work
+
+
+
+           //Example: [1,2,3,4];
+
+            $newProductId = [];
+            foreach ($request->items as $singleItem) {
+
+                $item = (object)$singleItem;
+                $newProductId[] = $item->product_id;
+                $purchaseItemInput = [
+                    'purchase_id' => $purchase->id,
+                    'product_id' => $item->product_id,
+                    'big_unit_price' => $item->big_unit_price ?? null,
+                    'small_unit_price' => $item->small_unit_price,
+                    'big_unit_qty' => $item->big_unit_qty ?? null,
+                    'small_unit_qty' => $item->small_unit_qty
+                ];
+
+                $purchaseItem = PurchaseItem::where(['purchase_id'=>$id,'product_id'=>$item->product_id])->first();
+                if($purchaseItem!=''){
+                    $purchaseItem->update($purchaseItemInput);
+                }else{
+                    $purchaseItem = PurchaseItem::create($purchaseItemInput);
+                }
+
+                // Start Inventory/Stock Update
+                $inventory = Inventory::where('product_id',$item->product_id)->first();
+
+                $inventoryChallan = InventoryChallan::where([
+                    'purchase_id'=>$purchase->id,
+                    'inventory_id'=>$inventory->id,
+                    'product_id'=>$item->product_id
+                ])->first();
+
+                if($inventory!=''){
+                                    //                  20                      -           12                     +    15 = 23
+                    $available_big_unit_qty = $inventory->available_big_unit_qty - $inventoryChallan->big_unit_qty + ($item->big_unit_qty??0);
+                    $available_small_unit_qty = $inventory->available_small_unit_qty - $inventoryChallan->small_unit_qty + $item->small_unit_qty;
+
+                    $inventoryInput = [
+                        'product_id'=>$item->product_id,
+                        'available_big_unit_qty'=>$available_big_unit_qty,
+                        'available_small_unit_qty'=>$available_small_unit_qty,
+                        'big_unit_sales_price'=>$item->big_unit_sales_price?? null,
+                        'small_unit_sales_price'=>$item->small_unit_sales_price,
+                    ];
+                    $inventory->update($inventoryInput);
+
+                    // Update Inventory Challan
+                    $inventoryChallanInput = [
+                        'purchase_id'=>$purchase->id,
+                        'inventory_id'=>$inventory->id,
+                        'product_id'=>$item->product_id,
+                        'big_unit_sales_price'=>$item->big_unit_sales_price?? null,
+                        'small_unit_sales_price'=>$item->small_unit_sales_price,
+                        'big_unit_cost_price'=>$item->big_unit_price?? null,
+                        'small_unit_cost_price'=>$item->small_unit_price,
+                        'big_unit_qty'=>$item->big_unit_qty?? null,
+                        'small_unit_qty'=>$item->small_unit_qty,
+                    ];
+                    $inventoryChallan->update($inventoryChallanInput);
+                }
+            }
+
+            //finding missing Items
+            $oldProductId = PurchaseItem::where('purchase_id',$id)->pluck('product_id')->toArray();
+            $missingProduct = array_diff($oldProductId,$newProductId);
+            $missingProductId = array_values($missingProduct);
+            PurchaseItem::whereIn('product_id',$missingProductId)->where('purchase_id',$id)->delete();
+            InventoryChallan::whereIn('product_id',$missingProductId)->where('purchase_id',$id)->delete();
+
+            DB::commit();
+            return response()->json("Successfully Updated",201);
+
+            }catch(Exception $e){
+            DB::rollback();
+            return response()->json(['error'=>$e->errorInfo[2]],500);
+        }
+
     }
 
     /**
